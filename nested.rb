2062e4c710211c57467c6431835d21f515f2899b
Nicholas Powers <njpowers7915@gmail.com>
2018-05-08 01:01:08 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-
+puts programmer_hash
 end
 
 def changing_alan
@@ -104,7 +104,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
   programmer_hash[:alan_kay][:known_for] = alans_new_info
-    
+  puts programmer_hash
 end
 
 def adding_to_dennis
@@ -123,7 +123,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 puts programmer_hash
 
 end
